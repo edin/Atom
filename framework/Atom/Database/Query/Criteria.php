fefa46f711c674991108789393a4e567916d979b
Edin Omeragic <edin.omeragic@gmail.com>
2019-10-01 00:21:35 +0200

    Join criteria builder

--- framework/Atom/Database/Query/Criteria.php
+++ framework/Atom/Database/Query/Criteria.php
@@ -1,14 +1,54 @@
 <?php
 
-namespace Atom\Database;
+namespace Atom\Database\Query;
+
+use Atom\Database\Query\Ast\BinaryExpression;
 
 class Criteria
 {
-    public function where(string $column, $value)
+    private $params = [];
+    private $expression = null;
+
+    private function combineExpression($operator, $column, $value, $isValue): self
     {
+        $exp = new BinaryExpression();
+        $exp->leftNode = $column;
+        $exp->rightNode = $value;
+
+        if ($this->expression == null) {
+            $this->expression = $exp;
+        } else {
+            $node = new BinaryExpression();
+            $node->operator = $operator;
+            $node->leftNode = $this->expression;
+            $node->rightNode = $exp;
+            $this->expression = $node;
+        }
+        return $this;
     }
 
-    public function orWhere(callabele $callable)
+    public function on(string $column, $value): self
     {
+        return $this->combineExpression("AND", $column, $value, false);
+    }
+
+    public function orOn(string $column, $value): self
+    {
+        return $this->combineExpression("OR", $column, $value, false);
+    }
+
+    public function where(string $column, $value): self
+    {
+        return $this->combineExpression("AND", $column, $value, true);
+    }
+
+    public function orWhere(string $column, $value): self
+    {
+        return $this->combineExpression("OR", $column, $value, true);
+    }
+
+    public function orGroup(callable $callable): self
+    {
+        return $this;
     }
 }
--- framework/Atom/Database/Query/JoinCriteria.php
+++ framework/Atom/Database/Query/JoinCriteria.php
@@ -3,55 +3,7 @@
 namespace Atom\Database\Query;
 
 use Atom\Database\Query\Ast\BinaryExpression;
-use Atom\Database\Query\Ast\UnaryExpression;
 
-class JoinCriteria
+class JoinCriteria extends Criteria
 {
-    private $expression = null;
-
-    private function combineExpression($operator, $column, $value, $isValue)
-    {
-        $exp = new BinaryExpression();
-        $exp->leftNode = $column;
-        $exp->rightNode = $value;
-
-        if ($this->expression == null) {
-            $this->expression = $exp;
-        } else {
-            $node = new BinaryExpression();
-            $node->operator = $operator;
-            $node->leftNode = $this->expression;
-            $node->rightNode = $exp;
-            $this->expression = $node;
-        }
-    }
-
-    public function on(string $column, $value): self
-    {
-        $this->combineExpression("AND", $column, $value, false);
-        return $this;
-    }
-
-    public function orOn(string $column, $value): self
-    {
-        $this->combineExpression("OR", $column, $value, false);
-        return $this;
-    }
-
-    public function where(string $column, $value): self
-    {
-        $this->combineExpression("AND", $column, $value, true);
-        return $this;
-    }
-
-    public function orWhere(string $column, $value): self
-    {
-        $this->combineExpression("OR", $column, $value, true);
-        return $this;
-    }
-
-    public function orGroup(callable $callable): self
-    {
-        return $this;
-    }
 }
