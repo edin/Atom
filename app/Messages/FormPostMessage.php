baf95f30fe02be2b735cb94862441de18fdb8cde
Edin Omeragic <edin.omeragic@gmail.com>
2020-03-08 20:50:20 +0100

    Removed template

--- app/Messages/FormPostMessage.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Messages;
-
-class FormPostMessage {
-    public $firstName;
-    public $lastName;
-}
-
--- app/Views/Home/item.php
+++
@@ -1,6 +0,0 @@
-<?php $view->extend("layout"); ?>
-
-<h2>Item details</h2>
-<div>
-    <a class="btn btn-sm btn-primary" href="<?= $baseUrl ?>">Home</a>
-</div>
