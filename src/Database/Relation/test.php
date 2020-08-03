1d26c98a6ca668ff49d34f7f8d0d301a788d74bf
Edin Omeragic <edin.omeragic@gmail.com>
2020-08-03 12:16:06 +0200

    Relation test file

--- src/Database/Relation/test.php
+++ src/Database/Relation/test.php
@@ -1,6 +1,6 @@
 <?php
 
-use Atom\Database\Relation\RelationBuilder;
+use Atom\Database\Mapping\Mapping;
 
 class Group
 {
@@ -21,15 +21,9 @@ class Role
  *  CREATE TABLE user_roles (ID INT, UserID int, RoleID int);
  */
 
-$relation = new RelationBuilder(User::class);
-
-$hasMany = $relation->hasMany(Role::class, "UserID");
-// Select ... From roles Where UserID = :userId
-
-$hasMany = $relation->hasManyTrough(Role::class, "user_roles", "UserID", "RoleID");
-// Select ... From roles r
-// Join user_roles ur 
-// Where ur.RoleID = r.ID AND ur.UserID = :userId
-
-$belongsTo = $relation->belongsTo(Group::class, "GroupID");         
-// Select ... From groups where Id = :groupId
+$mapping = new Mapping();
+$mapping->setEntityClass(User::class);
+$mapping->relation("roles")->hasMany(Role::class, "UserID");
+$mapping->relation("rolesTrough")->hasManyTrough(Role::class, "UserID", "user_roles", "UserID", "RoleID");
+$mapping->relation("group")->belongsTo(Group::class, "GroupID");
+$relations = $mapping->getRelations();
